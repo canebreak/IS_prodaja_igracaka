1c75c2c198f4a982494136d0267da26a7d989c2b
cane <bstankovic95@gmail.com>
2017-08-26 15:20:51 +0200

    Korisnici dodati, popunjena baza do kraja.

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,17 +1,16 @@
+
 package code;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.sql.*;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -28,6 +27,7 @@ public class Main extends Application {
 
         Date today = Calendar.getInstance().getTime();
         DB db = DB.getInstance();
+        ArrayList<String> listaRadnikaKojiSuPrijaviliProbleme = new ArrayList<>();
         Radnik radnik = new Radnik();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         String date = sdf.format(today);
@@ -42,42 +42,17 @@ public class Main extends Application {
         Button login = new Button("Uloguj se kao radnik");
         Button basicUser = new Button("Nastavi kao korisnik");
 
-        //scene for logged starts here
 
+        VBox box = new VBox();
+
+        box.getChildren().addAll(username,usernameTxt,pass,passTxt, login, basicUser);
+        box.setSpacing(10);
+
+        Scene scene = new Scene(box, 400,375);
+        window.setScene(scene);
         VBox ulogovanVBox = new VBox(10);
-
-        TextArea opisProblema = new TextArea();
-        opisProblema.setMinHeight(200);
-        opisProblema.setMinWidth(200);
-        opisProblema.setMaxWidth(250);
-        opisProblema.setPromptText("Opis problema sa strujom, kasom, tehnikom...");
-
-        Button posaljiProblem = new Button("Posalji problem");
-
-        posaljiProblem.setOnAction(event -> {
-
-            Connection con = null;
-            Statement stmt = null;
-            try {
-                con = db.getConnection();
-                stmt = con.createStatement();
-                String upit = "INSERT INTO `Problemi`(`id_tiketa`, `id_prijavnika`, `datum`, `opis`, `id_lokacije`) VALUES (NULL, "+radnik.getId()+",'"+date+"', '"+opisProblema.getText()+"', (" +
-                        "SELECT id_lokacije from rbp.Lokacija where grad='"+radnik.getLokacija()+"'))";
-                stmt.executeUpdate(upit);
-                System.out.println("query prosao");
-            }catch (SQLException e)
-            {
-                System.out.println(e.getMessage());
-            }
-        });
-
-        ulogovanVBox.getChildren().addAll(opisProblema, posaljiProblem);
-
-
         Scene ulogovanUser = new Scene(ulogovanVBox, 400,375);
 
-
-        //scene for logged ends here
         login.setOnAction(event -> {
 
             Connection con = null;
@@ -114,14 +89,112 @@ public class Main extends Application {
 
 
 
-        VBox box = new VBox();
+        //scene for logged starts here
 
-        box.getChildren().addAll(username,usernameTxt,pass,passTxt, login, basicUser);
-        box.setSpacing(10);
 
-        Scene scene = new Scene(box, 400,375);
-        window.setScene(scene);
+        TextArea opisProblema = new TextArea();
+        opisProblema.setMinHeight(200);
+        opisProblema.setMinWidth(200);
+        opisProblema.setMaxWidth(250);
+        opisProblema.setPromptText("Opis problema sa strujom, kasom, tehnikom...");
 
+        Button posaljiProblem = new Button("Posalji problem");
+
+
+        posaljiProblem.setOnAction(event -> {
+
+            Connection con = null;
+            Statement stmt = null;
+            try {
+                con = db.getConnection();
+                stmt = con.createStatement();
+                String upit = "INSERT INTO `Problemi`(`id_tiketa`, `id_prijavnika`, `datum`, `opis`, `id_lokacije`) VALUES (NULL, "+radnik.getId()+",'"+date+"', '"+opisProblema.getText()+"', (" +
+                        "SELECT id_lokacije from rbp.Lokacija where grad='"+radnik.getLokacija()+"'))";
+                stmt.executeUpdate(upit);
+                System.out.println("query prosao");
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Uspesno prijavljen problem");
+                alert.setHeaderText("Uspesno prijavljeno sluzbi u gradu " + radnik.getLokacija());
+                alert.setContentText("Hvala na prijavi!");
+                alert.showAndWait();
+
+                window.setScene(scene);
+                usernameTxt.setText("");
+                passTxt.setText("");
+            }catch (SQLException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        });
+
+        ulogovanVBox.getChildren().addAll(opisProblema, posaljiProblem);
+
+
+
+
+        //scene for logged ends here
+
+
+        //scene for basic user starts here
+        VBox vBoxZaObicneKorisnike = new VBox(20);
+
+        Label opis = new Label("Ukoliko zelite da vidite radnike iz posebnog grada, unesite u polje");
+
+        TextField poljeZaGrad = new TextField();
+        poljeZaGrad.setMaxWidth(100);
+        poljeZaGrad.setPromptText("Polje za grad...");
+        Button radniciProblemi = new Button("Prikazi radnike koji su prijavili probleme");
+
+        Button najskupljaIgracka = new Button("Prikazi najskuplju igracku");
+        Button najjeftinijaIgracka = new Button("Prikazi najjeftiniju igracku");
+
+        Button prodajKupiIgracku = new Button("Prodaj/kupi igracku");
+        vBoxZaObicneKorisnike.getChildren().addAll(opis,poljeZaGrad,radniciProblemi, najskupljaIgracka, najjeftinijaIgracka, prodajKupiIgracku);
+
+        Scene scenaZaObicneKorisnike = new Scene(vBoxZaObicneKorisnike, 450, 350);
+        //scene for basic user ends here
+
+
+
+
+
+
+
+
+
+        basicUser.setOnAction(event -> {window.setScene(scenaZaObicneKorisnike);});
+        radniciProblemi.setOnAction(event -> {
+            Connection con= null;
+            Statement stmt = null;
+            ResultSet rs = null;
+            String text = poljeZaGrad.getText();
+            String upit = "";
+            try
+            {
+                con = DB.getInstance().getConnection();
+                stmt = con.createStatement();
+                if(text.length()>3)
+                {
+                    upit = "SELECT ime from Zaposleni WHERE id_radnika IN (SELECT id_prijavnika from Problemi)";
+                }
+                else
+                    upit = "SELECT ime from Zaposleni WHERE lokacija like '%"+text+"%' AND id_radnika IN (SELECT id_prijavnika from Problemi)";
+                rs = stmt.executeQuery(upit);
+                while(rs.next())
+                {
+                    listaRadnikaKojiSuPrijaviliProbleme.add(rs.getString("ime"));
+                }
+                for(int i = 0;i<listaRadnikaKojiSuPrijaviliProbleme.size();i++)
+                {
+                    System.out.println(listaRadnikaKojiSuPrijaviliProbleme.get(i));
+                }
+            }catch(SQLException e)
+            {
+
+            }
+
+        });
         window.show();
 
 
